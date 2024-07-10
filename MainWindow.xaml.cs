a9aae73a18f7ee814a6000b84b12d4a3c5d4982f
Omkinus <omkinus@gmail.com>
2024-07-10 11:43:17 +0300

    firsttests

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Tekla.Structures.Drawing;
+using tsm = Tekla.Structures.Model;
+using Tekla.Structures.Drawing.UI;
+using Tekla.Structures.Dialog.UIControls;
+using Tekla.Structures.Model;
+using Tekla.Structures.Catalogs;
+using Part = Tekla.Structures.Model.Part;
 
 namespace AttCleaner
 {
@@ -23,11 +30,39 @@ namespace AttCleaner
         public MainWindow()
         {
             InitializeComponent();
+            this.MouseDown += delegate { DragMove(); };
+
         }
+
+        tsm.Model _model = new tsm.Model();
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (!_model.GetConnectionStatus())
+            {
+                MessageBox.Show("ВКЛЮЧИ ТЕКЛУ");
+            }
+
+            ModelObjectEnumerator selectedModelObjects = new Tekla.Structures.Model.UI.ModelObjectSelector().GetSelectedObjects();
+
+
+
+            foreach (var item in selectedModelObjects)
+            {
+                if (item is Part) {
+                    Tekla.Structures.Model.ModelObject modelObject = item as Tekla.Structures.Model.ModelObject;
+                    modelObject.SetUserProperty(attributetextbox.Text,"");
+                    modelObject.Modify();
+                }
+
+            }
+
+
         }
     }
 }
